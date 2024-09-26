b8edd47911c2f172d1370894b72865cad9966ac1
Venkatesh7981 <159419272+Venkatesh7981@users.noreply.github.com>
2024-09-26 17:07:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a row-column sorted Matrix/search-in-a-rowcolumn-sorted-matrix.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG{
+	public static void main(String args[]) throws IOException { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t > 0){
+       		int rows=sc.nextInt();
+       		int columns=sc.nextInt();
+			
+			int matrix[][]=new int[rows][columns];
+          
+        	for(int i=0; i<rows;i++){            
+            	for(int j=0; j<columns;j++){
+                	matrix[i][j]=sc.nextInt();
+            	}
+         	}
+         	int target = sc.nextInt();
+         	
+			Solution x = new Solution();
+			
+			if (x.search(matrix, rows, columns, target)) 
+				System.out.println(1); 
+			else
+				System.out.println(0); 
+			t--;
+		}
+	} 
+}
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{ 
+    //Function to search a given number in row-column sorted matrix.
+	static boolean search(int matrix[][], int n, int m, int x) 
+	{  
+	    // code here 
+	    if(matrix==null || matrix.length<1|| matrix[0].length<1){
+	        return false;
+	    }
+         int col=matrix[0].length-1;
+       int row=0;
+       while(col>=0 && row<=matrix.length-1){
+        if(x==matrix[row][col]){
+            return true;
+        }
+        else if(x<matrix[row][col]){
+            col--;
+        }
+        else row++;
+       }
+       return false;
+	} 
+} 
