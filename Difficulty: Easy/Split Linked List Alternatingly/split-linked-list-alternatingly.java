815c65bd108b7fa012201f47d498a0bf13e57991
Venkatesh7981 <159419272+Venkatesh7981@users.noreply.github.com>
2024-10-17 19:27:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            Scanner ss = new Scanner(input);
+
+            while (ss.hasNextInt()) {
+                arr.add(ss.nextInt());
+            }
+
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+
+            Solution ob = new Solution();
+            Node[] result = ob.alternatingSplitList(head);
+            printList(result[0]);
+            printList(result[1]);
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    // Function to append a new node with newData at the end of a linked list
+    Node[] alternatingSplitList(Node head) {
+        // code here
+           Node h2 = null;
+        if(head!= null && head.next != null) {
+            h2 = head.next;
+        }
+        Node head1 = head;
+        Node head2 = h2;
+        
+        while(head.next != null && h2.next != null) {
+             head.next = head.next.next;
+             h2.next = h2.next.next;
+             head = head.next;
+             h2 = h2.next;
+        }
+        head.next = null;
+        Node [] arr = new Node[2];
+        arr[0] = head1;
+        arr[1] = head2;
+        return arr;
+    }
+}
