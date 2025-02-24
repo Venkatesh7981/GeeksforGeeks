a914e2d446efddbf3cb0c8c7a953f128c14bbd89
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2025-02-24 18:05:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -1,57 +1,64 @@
 //{ Driver Code Starts
+// Initial Template for Java
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-
-// } Driver Code Ends
-
-class Solution {
-    // Function to calculate the span of stockâ€™s price for all n days.
-    public static int[] calculateSpan(int price[], int n) {
-        // Your code here
-         Stack<Integer> st = new Stack<>();
-             int[] res = new int[n];
-             st.push(0);
-             res[0]=1;
-             for(int i=1;i<n;i++){
-                 while(!st.isEmpty()&& price[st.peek()]<=price[i]){
-                     st.pop();
-                  }
-                 if (st.isEmpty()) {
-                  res[i] = i + 1;
-                  } 
-                 else {
-                 res[i] = i - st.peek();
-                 }
-                st.push(i);
-             }
-             return res;
-    }
-}
-
-//{ Driver Code Starts.
-
-class gfg {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
 
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int a[] = new int[n];
 
-            int i = 0;
-            for (i = 0; i < n; i++) a[i] = sc.nextInt();
-
-            int[] s = new Solution().calculateSpan(a, n);
-
-            for (i = 0; i < n; i++) {
-                System.out.print(s[i] + " ");
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
             }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            v = new Solution().calculateSpan(arr);
+
+            for (int i = 0; i < v.size(); i++) System.out.print(v.get(i) + " ");
 
             System.out.println();
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
+
+
+
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        
+        Stack<Integer>s=new Stack<>();
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+           
+            while(!s.isEmpty()&&arr[s.peek()]<=arr[i])
+            {
+                s.pop();
+            }
+            if(!s.isEmpty())
+            {
+                ans.add(i-s.peek());
+            }
+            else{
+                ans.add(i+1);
+            }
+            s.add(i);
+        }
+        return ans;
+    }
+}
