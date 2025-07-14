d9ffa728c7210d68c2bbc7599e9b6b193f200e8e
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2025-07-14 18:22:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Panagram Checking/panagram-checking.java
+++ Difficulty: Easy/Panagram Checking/panagram-checking.java
@@ -1,46 +1,16 @@
-//{ Driver Code Starts
-//Initial template for JAVA
+// User function template for JAVA
 
-import java.lang.*;
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-//User function template for JAVA
-
-class Solution
-{
-    //Function to check if a string is Pangram or not.
-    public static boolean checkPangram  (String s) {
+class Solution {
+    // Function to check if a string is Pangram or not.
+    public static boolean checkPangram(String s) {
         // your code here
-               String str = "abcdefghijklmnopqrstuvwxyz";
-         s = s.toLowerCase();
-        for(int i=0; i<26; i++){
-          char ch = str.charAt(i);
-          
-          if(!s.contains(ch+"") )
-              return false;
-          }
-        return true;
+     String str="abcdefghijklmnopqrstuvwxyz";
+     s=s.toLowerCase();
+     for(int i=0;i<26;i++){
+         char ch=str.charAt(i);
+         if(!s.contains(ch+"")) return false;
+         
+     }
+     return true;
     }
 }
-
-//{ Driver Code Starts.
-
-class GFG
- {
-	public static void main (String[] args) throws IOException
-	 {
-	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	  int t=Integer.parseInt(br.readLine());
-	  while(t-->0)
-	  {
-	    String s=br.readLine().trim();
-	    
-	    System.out.println(new Solution().checkPangram (s)==true?1:0);
-	  }
-	  
-	 }
-}
-// } Driver Code Ends
